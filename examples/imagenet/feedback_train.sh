92f099a77604dc0678f9bd915e3179425f50c202
liuxianming <liuxianming@gmail.com>
2014-08-05 21:51:50 -0500

    samples and models for feedback training

---
+++ examples/imagenet/feedback_train.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+TOOLS=../../build/tools
+
+GLOG_logtostderr=1 $TOOLS/finetune_net_feedback.bin  alexnet_feedback_solver.prototxt ./caffe_alexnet_model 2>&1 |tee feedback.log
+
+echo "Done."
